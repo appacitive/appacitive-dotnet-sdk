4f6433f68976eb745ab7836d0f37064d7a7f2c37
Nikhil Prasad <nikhil.prasad@gmail.com>
2014-04-28 15:35:53 +0530

    Fixed namespace for WindowsPhone8 Platform helper class.

--- src/Appacitive.Sdk.WindowsPhone8/Platforms.cs
+++ src/Appacitive.Sdk.WindowsPhone8/Platforms.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Appacitive.Sdk.WindowsPhone8;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Appacitive.Sdk.WindowsPhone8
+namespace Appacitive.Sdk
 {
     public static class Platforms
     {
