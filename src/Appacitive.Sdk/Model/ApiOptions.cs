bf3f577cf41587052169da3ed89a0e2cc9f57381
Nikhil Prasad <nikhil.prasad@gmail.com>
2014-05-15 22:25:34 -0500

    Added environment support in api options.

--- src/Appacitive.Sdk/Model/ApiOptions.cs
+++ src/Appacitive.Sdk/Model/ApiOptions.cs
@@ -1,4 +1,5 @@
-﻿using Appacitive.Sdk.Services;
+﻿using Appacitive.Sdk.Internal;
+using Appacitive.Sdk.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,14 @@ namespace Appacitive.Sdk
 {
     public class ApiOptions
     {
+
+        public ApiOptions()
+        {
+            // Initialize the environment with the same value as currently set;
+            if( InternalApp.IsInitialized == true )
+                this.Environment = InternalApp.Current.Environment;
+        }
+
         internal static T Apply<T>(T request, ApiOptions options)
             where T : ApiRequest
         {
@@ -18,11 +27,14 @@ namespace Appacitive.Sdk
                 request.ApiKey = options.ApiKey;
             if (string.IsNullOrWhiteSpace(options.UserToken) == false)
                 request.UserToken = options.UserToken;
+            request.Environment = options.Environment;
             return request;
         }
 
         public string ApiKey { get; set; }
 
         public string UserToken { get; set; }
+
+        public Environment Environment { get; set; }
     }
 }
