7283cb6e5781f9bb85f0fe5312926dde85b07db3
Nikhil Prasad <nikhil.prasad@gmail.com>
2014-07-16 03:05:23 +0530

    Added release binaries for Appacitive SDK v1.0.

--- src/CommonAssembly.cs
+++ src/CommonAssembly.cs
@@ -9,7 +9,7 @@ using System;
 // associated with an assembly.
 [assembly: AssemblyCompany("Appacitive Software Private Limited")]
 [assembly: AssemblyProduct("Appacitive.Sdk")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -24,5 +24,5 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
 // [assembly: AssemblyFileVersion("0.9.1.*")]
