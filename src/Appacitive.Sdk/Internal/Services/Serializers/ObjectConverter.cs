46eed5fc88810a4031dcf56385902f713ac84b09
Nikhil Prasad <nikhil.prasad@gmail.com>
2014-09-08 13:21:10 +0530

    Fixed bug: Fixed APUser serialization bug for asp.net apps.

--- src/Appacitive.Sdk/Internal/Services/Serializers/ObjectConverter.cs
+++ src/Appacitive.Sdk/Internal/Services/Serializers/ObjectConverter.cs
@@ -38,6 +38,8 @@ namespace Appacitive.Sdk.Services
 
         private APObject BuildNewInstance(JObject json, Type objectType)
         {
+            // What is the spec for building a new instance
+            // We look for type in the json. If it does not exist then we defer to the type passed.
             string type;
             type = GetType(json);
             ICreateInstanceBehaviorFactory factory = ObjectFactory.Build<ICreateInstanceBehaviorFactory>();
@@ -48,7 +50,7 @@ namespace Appacitive.Sdk.Services
         {
             JToken value;
             if (json.TryGetValue("__type", out value) == false || value.Type == JTokenType.Null)
-                throw new Exception("Object type is missing.");
+                return null;
             var type = value.ToString();
             return type;
         }
@@ -85,9 +87,9 @@ namespace Appacitive.Sdk.Services
     {
         public ICreateInstanceBehavior GetBehavior(string type, Type instanceType)
         {
-            if (type.Equals("device") == true || instanceType.Is<APDevice>() == true)
+            if ("device".Equals(type) == true || instanceType.Is<APDevice>() == true)
                 return new CreateDeviceBehavior();
-            else if (type.Equals("user") == true || instanceType.Is<APUser>() == true)
+            else if ("user".Equals(type) == true || instanceType.Is<APUser>() == true)
                 return new CreateUserBehavior();
             else return new CreateObjectBehavior();
         }
